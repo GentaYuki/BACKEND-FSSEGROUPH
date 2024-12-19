37fe5f4cb2b92688e03e9a9ac91de5f8b8e08b80
Genta <genta.yuki@gmail.com>
2024-12-19 22:54:20 +0700

    update transaction

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -186,7 +186,7 @@ def create_order_transaction():
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        return response
+        return response, 200
 
     except Exception as e:
         response = jsonify({"success": False, "message": str(e)})
